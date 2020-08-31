4b00a32107a7f2fe42d3828555c25f45e5c34abe
Dhrumil Dalwadi <58628892+dhrumildalwadi@users.noreply.github.com>
2020-08-31 19:41:00 +0530

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -1,48 +1,39 @@
-import os
-import numpy as np 
-from PIL import Image 
-import cv2
 import pickle
+import os,cv2
+import numpy as np
+from PIL import Image;
+import os
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+detector= cv2.CascadeClassifier("haarcascade_frontalface_default.xml");
 
-faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-recognizer = cv2.face.createLBPHFaceRecognizer()
-
-baseDir = os.path.dirname(os.path.abspath(__file__))
-imageDir = os.path.join(baseDir, "images")
-
-currentId = 1
-labelIds = {}
-yLabels = []
-xTrain = []
-
-for root, dirs, files in os.walk(imageDir):
-	print(root, dirs, files)
-	for file in files:
-		print(file)
-		if file.endswith("png") or file.endswith("jpg"):
-			path = os.path.join(root, file)
-			label = os.path.basename(root)
-			print(label)
-
-			if not label in labelIds:
-				labelIds[label] = currentId
-				print(labelIds)
-				currentId += 1
-
-			id_ = labelIds[label]
-			pilImage = Image.open(path).convert("L")
-			imageArray = np.array(pilImage, "uint8")
-			faces = faceCascade.detectMultiScale(imageArray, scaleFactor=1.1, minNeighbors=5)
-
-			for (x, y, w, h) in faces:
-				roi = imageArray[y:y+h, x:x+w]
-				xTrain.append(roi)
-				yLabels.append(id_)
-
-with open("labels", "wb") as f:
-	pickle.dump(labelIds, f)
-	f.close()
-
-recognizer.train(xTrain, np.array(yLabels))
-recognizer.save("trainer.yml")
-print(labelIds)
+def getImagesAndLabels(path):
+    dirlist = [ item for item in os.listdir(path) if os.path.isdir(os.path.join(path, item)) ]
+    faceSamples=[]
+    count_id=[]
+    name_and_ids={}
+    for name in dirlist:
+        Ids=[]
+        imagePaths=[os.path.join(path+name+"/",f) for f in os.listdir(path+name+"/")]
+        for imagePath in imagePaths:
+        	#loading the image and converting it to gray scale
+        	pilImage=Image.open(imagePath).convert('L')
+        	#Now we are converting the PIL image into numpy array
+        	imageNp=np.array(pilImage,'uint8')
+        	#getting the Id from the image
+        	Id=int(os.path.split(imagePath)[-1].split(".")[1])
+        	# extract the face from the training image sample
+        	faces=detector.detectMultiScale(imageNp)
+        	for (x,y,w,h) in faces:
+            		faceSamples.append(imageNp[y:y+h,x:x+w])
+            		count_id.append(Id)
+            		Ids.append(Id)
+        name_and_ids.update({name:Ids})
+    return faceSamples,count_id,name_and_ids
+faces,Ids,name = getImagesAndLabels('images/')
+print(name)
+print(Ids)
+with open("labels","wb" ) as f:
+ 	pickle.dump(name,f)
+recognizer.train(faces,np.array(Ids))
+print("Successfully trained")
+recognizer.save('trainer.yaml')
